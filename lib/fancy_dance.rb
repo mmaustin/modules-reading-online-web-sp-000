43b51ab5b1e8e6c619f85c2e05aa80ca7d064232
mccray austin <tubach75@hotmail.com>
2020-10-25 14:50:29 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,6 +16,7 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
+    
   end
 
   module ClassMethods
@@ -23,6 +24,7 @@ module FancyDance
     def metadata
       "This class produces objects that love to dance."
     end
+    
   end
   
 end
